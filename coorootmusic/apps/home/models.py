560924f712aae4ccc3db45a1410e3a871c085d62
akmatoff <azim.akmatov1@gmail.com>
2019-11-13 23:02:12 +0600

    Created a model

--- coorootmusic/apps/home/models.py
+++ coorootmusic/apps/home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class About(models.Model):
+    about_title = models.CharField('About Name', max_length = 200)
+    about_text = models.TextField('About Text')
+
+    def __str__(self):
+        return self.about_title
+
+    def __str__(self):
+        return self.about_text
+    
--- coorootmusic/apps/releases/views.py
+++ coorootmusic/apps/releases/views.py
@@ -2,6 +2,3 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'releases/index.html')
-
-def home(request):
-    return render(request, 'home/home.html')
